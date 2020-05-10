2f46c2b06f672265d0f51803609df0e5df506c9c
Morgan Webb <mnbwebb2233@gmail.com>
2020-05-10 22:18:47 +1000

    Create Repo Model

---
+++ db/migrate/20200510121701_create_repos.rb
@@ -0,0 +1,24 @@
+class CreateRepos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :repos do |t|
+      t.string :pkgname
+      t.string :pkgbase
+      t.string :repo
+      t.string :arch
+      t.integer :pkgver
+      t.integer :pkgrel
+      t.integer :epoch
+      t.text :pkgdesc
+      t.string :url
+      t.string :filename
+      t.integer :compressed_size
+      t.integer :installed_size
+      t.date :build_date
+      t.date :last_update
+      t.date :flag_date
+      t.string :packager
+
+      t.timestamps
+    end
+  end
+end
